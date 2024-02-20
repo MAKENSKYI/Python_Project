5d247a1014903cf4c870394415055920c51d212a
MAKENSKYI <m_oteuli@kbtu.kz>
2024-02-20 22:25:32 +0600

    TSIS4

---
+++ lab3/444.py
@@ -0,0 +1,12 @@
+class person():
+    def __init__ (self, name, grade):
+        self.name = name
+        self.grade = grade
+p1 = person("John", 5)
+p2 = person("Magzhan", 6)
+p3 = person("Erasyl", 4)
+p4 = person("Nurislam", 8)
+
+
+
+print(p2.grade)
