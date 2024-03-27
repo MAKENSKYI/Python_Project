2707fb0ecd358435b225de2ea038709ddb5fe66a
MAKENSKYI <m_oteuli@kbtu.kz>
2024-03-28 00:04:52 +0600

    7tsis

--- lab4/json/1.py
+++ lab4/json/1.py
@@ -1,9 +1,14 @@
-import json 
-print("""Interface Status
-================================================================================
-DN                                  x.                Description           Speed    MTU  
--------------------------------------------------- --------------------  ------  ------""")
-with open('D:/git/pp2/TSIS 4/Json/sample-data.json ',"r")as file:
-    data= json.load(file)
-    for item in data['imdata']:
-        print(f"{item['l1PhysIf']['attributes']['dn']}                               {item['l1PhysIf']['attributes']['speed']}  {item['l1PhysIf']['attributes']['mtu']}")
+import re
+
+txt = open ("berr.txt", "r")
+
+
+x = re.search("[A-Z]", txt)
+
+print(x)
+
+if x:
+  print("Yes, there is at least one match!")
+else:
+  print("No match")
+  
